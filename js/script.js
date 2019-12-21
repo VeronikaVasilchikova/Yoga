e62104dd8332eb4c7c6bfc7e360cdc657b4bfb29
VeronikaVasilchikova <veroni4ka.v@gmail.com>
2019-12-21 19:42:57 +0300

    create a timer

--- js/script.js
+++ js/script.js
@@ -36,4 +36,42 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    // create a Timer
+
+    let deadline = '2019-12-31';
+
+    function getTimeRemaining(endtime) {
+        let totalTime = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((totalTime / 1000) % 60),
+            minutes = Math.floor((totalTime / 1000 / 60) % 60),
+            hours = Math.floor((totalTime / (1000*60*60)));
+        return {
+            'totalTime': totalTime,
+            'seconds': seconds,
+            'minutes': minutes,
+            'hours': hours,
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if(t.totalTime <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('timer', deadline);
+
 });
